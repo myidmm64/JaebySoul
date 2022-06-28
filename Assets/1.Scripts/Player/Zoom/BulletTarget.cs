671e94e2206747b50e04b06b201d6b7cf8d6436f
myidmm64 <72392141+myidmm64@users.noreply.github.com>
2022-06-28 10:35:23 +0900

    모두 지울

--- Assets/1.Scripts/Player/Zoom/BulletTarget.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//불렛에 맞으면 다른 작용을 하게 하는 스크립트, 갖고만 있으면 됨
-public class BulletTarget : MonoBehaviour
-{
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# EngineRPGTPS
-엔진시간 마지막 수행평가
